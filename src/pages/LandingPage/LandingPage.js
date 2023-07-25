33b8b9ec4344986ab9074b384991175fc29fbde1
Chanathip Gulgalkhai <kenobi1337@protonmail.com>
2023-07-25 12:54:05 -0400

    add animation to landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,6 +3,7 @@ import { Button } from "@mui/material";
 import styled from "styled-components";
 import shophowellLandingPageBackground from '../../images/shophowell_landing_page_bg.png';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const LandingPageContainer = styled.div`
   width: 100%;
@@ -107,9 +108,9 @@ const Text = styled.span`
 function LandingPage() {
     return (
         <LandingPageContainer>
-            <Blur>
-                <Navbar>
-                    <NexusLogo>
+            <Blur as={motion.div} initial={{opacity: 0 }} animate={{opacity: 1}} transition={{duration: 1.3}}>
+                <Navbar as={motion.div} initial={{x: "-100px"}} animate={{x: 0}} transition={{ duration: 0.8, ease: "easeInOut" }}>
+                    <NexusLogo >
                         <svg xmlns="http://www.w3.org/2000/svg" width="59" height="58" viewBox="0 0 59 58" fill="none">
                             <path d="M8.27615 14.5913L21.3326 35.3107L6.64355 49.8644H17.1362L24.6928 40.6435L34.7113 56.5414H41.6439L15.2093 14.5913H8.27615Z" fill="white"/>
                             <path d="M55.5785 1.4585L27.3057 29.4999L29.935 33.6174L55.5785 1.4585Z" fill="white"/>
@@ -166,7 +167,7 @@ function LandingPage() {
 
                 </Navbar>
                 <Content>
-                    <div>
+                    <motion.div initial={{y: "-100px", opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 1}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="243" height="83" viewBox="0 0 243 83" fill="none">
                             <g filter="url(#filter0_d_57_1957)">
                                 <path d="M23.4693 52.1909H23.3163L15.1994 20.0688H4.73079V73.733H12.1667V34.5566H12.3168L22.3295 73.733H30.9023V20.0688H23.4693V52.1909Z" fill="white"/>
@@ -206,10 +207,10 @@ function LandingPage() {
                                 </filter>
                             </defs>
                         </svg>
-                    </div>
-                    <Intro>Welcome To The Nexus!</Intro>
-                    <SubIntro>Let’s see where you are in your e-commerce journey!</SubIntro>
-                    <Button color="secondary">
+                    </motion.div>
+                    <Intro as={motion.p} initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1.55}}>Welcome To The Nexus!</Intro>
+                    <SubIntro as={motion.p} initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1.7}}>Let’s see where you are in your e-commerce journey!</SubIntro>
+                    <Button color="secondary" as={motion.div} initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1.9}}>
                     <Link to="/home" style={{ textDecoration: 'none' }}><LandingButton>
                         <Text>Start Now</Text>
                     </LandingButton>
