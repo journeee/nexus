3780db4ab1383313dac2ee90d3364537e68a739c
journeee <85528326+journeee@users.noreply.github.com>
2023-10-20 11:00:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
-import { Routes, Route } from "react-router-dom"
-
-// pages
-import LandingPage from './pages/LandingPage/LandingPage';
-import HomePage from "./pages/HomePage/HomePage";
-import SignupPage from './pages/SignupPage/Signup';
-import SigninPage from './pages/SigninPage/Signin';
-import Dashboard from './pages/DashboardPage/Dashboard';
-
+const LandingPage = lazy(() => import('./pages/LandingPage/LandingPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const SignupPage = lazy(() => import('./pages/SignupPage/Signup'));
+const SigninPage = lazy(() => import('./pages/SigninPage/Signin'));
+const Dashboard = lazy(() => import('./pages/DashboardPage/Dashboard'));
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <div className="mobile-menu">
+          <input type="checkbox" id="menu-toggle" />
+          <label htmlFor="menu-toggle" className="menu-icon">
+            <span></span>
+          </label>
+          <ul className="menu-items">
+            <li>
+              <Link to="/">Landing</Link>
+            </li>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li>
+              <Link to="/signin">Signin</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-            <Route path="/" element={<LandingPage />}/>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/signup" element={<SignupPage />}/>
-            <Route path="/signin" element={<SigninPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
